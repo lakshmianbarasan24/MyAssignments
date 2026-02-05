0c82458c107312363dfdd19a0cdbcb7772feee45
lakshmianbarasan24 <lakshmi.anbarasan24@gmail.com>
2026-02-05 15:17:51 +0530

    Day1-Understanding Data Types and Variables

---
+++ week1/day1/EmployeeDetails.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "John Doe";
+		int age = 30;
+		float salary = 55000.75f;
+		char gender = 'M';
+		boolean acceptTerms = true;
+		float rating = 4.5f;
+		long mobileNumber = 9876543210l;
+		int pinCode = 56001;
+		int luckyNumber = 7;
+		
+		System.out.println("Name: " + name);
+		System.out.println("Age: " + age);
+		System.out.println("Salary: " + salary);
+		System.out.println("Gender: " + gender);
+		System.out.println("Accept Terms: " + acceptTerms);
+		System.out.println("Rating: " + rating);
+		System.out.println("Mobile Number: " + mobileNumber);
+		System.out.println("PinCode: " + pinCode);
+		System.out.println("Lucky Number: " + luckyNumber);
+	}
+
+}
